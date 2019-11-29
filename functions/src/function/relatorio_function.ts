9131dfb5548e4b091f2e13ef5d89351b493c7bf4
Prof Catalunha <catalunha.mj@gmail.com>
2019-11-29 18:11:45 -0300

    listadeencontros em desenvolvimento

--- functions/src/function/relatorio_function.ts
+++ functions/src/function/relatorio_function.ts
@@ -5,6 +5,7 @@
 import DatabaseReferences from "../database-references";
 import { construirListaDeAlunosDaTurma } from "./relatorios_function/lista_alunos_turma";
 import { construirListaDeTarefasDoAluno } from "./relatorios_function/lista_notas_aluno";
+import { construirListaDeEncontros } from "./relatorios_function/lista_encontros";
 
 const express = require('express');
 const cors = require('cors')({ origin: true });
@@ -70,5 +71,36 @@ app.get('/listadetarefasdoaluno', (request: any, response: any) => {
         });
     }).catch((err) => {
         response.status(403).send('Desculpe. Não existe pedido autenticado para ListaDeTarefasDoAluno.' + err);
+    });
+});
+
+
+// /listadeencontros?pedido=<relatorioId>
+app.get('/listadetarefasdoaluno', (request: any, response: any) => {
+    let pedidoId = request.query.pedido;
+    console.log("ListaDeEncontros :: Pedido id: ", pedidoId);
+    DatabaseReferences.db.collection('Relatorio').doc(pedidoId).get().then((docRelatorio: any) => {
+        if (!docRelatorio.exists) {
+            console.log('Desculpe. Collection Relatorio ou documento não encontrado para ListaDeEncontros.');
+            throw new Error("Desculpe. Collection Relatorio ou documento não encontrado para ListaDeEncontros.");
+        }
+        let relatorio = docRelatorio.data();
+        console.log("ListaDeEncontros. Relatorio id: " + docRelatorio.id);
+        construirListaDeEncontros(relatorio.turmaId).then((planilha) => {
+            const csv = json2csv(planilha);
+            response.setHeader(
+                "Content-disposition",
+                "attachment; filename=ListaDeEncontros.csv"
+            );
+            response.set("Content-Type", "text/csv");
+            response.status(200).send(csv);
+            DatabaseReferences.db.collection('Relatorio').doc(pedidoId).delete().then(() => {
+                console.log("ListaDeEncontros. Deletado  pedidoId: " + pedidoId);
+            })
+        }).catch((err) => {
+            response.status(403).send('Desculpe. Não foi possível construir a ListaDeEncontros' + err);
+        });
+    }).catch((err) => {
+        response.status(403).send('Desculpe. Não existe pedido autenticado para ListaDeEncontros.' + err);
     });
 });
